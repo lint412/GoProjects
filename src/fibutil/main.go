fd9a7639b4a96a0789f9d3c1b82e69b7fc9f108b
lint412 <13906512929@163.com>
2016-01-20 15:55:38 +0800

    Add func using

--- src/fibutil/main.go
+++ src/fibutil/main.go
@@ -2,17 +2,19 @@
 package main
 
 import (
-	"fibutil/fib"
+	//	"fibutil/fib"
 	"fmt"
-	//	"os"
+	"os"
 	//	"strconv"
 )
 
 func main() {
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(fib.Fibm(int64(i)))
-	}
+	//	for i := 0; i < 10; i++ {
+	//		fmt.Println(fib.Fibm(int64(i)))
+	//	}
+
+	goosInfo()
 
 	//	switch len(os.Args) {
 	//	case 1:
@@ -33,4 +35,11 @@ func main() {
 	//		}
 	//	}
 	//	fmt.Fprintf(os.Stderr, "%s, fibonacci number util\n\tfibutil n\t:fibonacci number\n\tfibutil n1 n2\t:fibonacci number list\n", os.Args[0])
+}
+
+func goosInfo() {
+	var goos string = os.Getenv("GOOS")
+	fmt.Printf("The operating system is: %s\n", goos)
+	path := os.Getenv("PATH")
+	fmt.Printf("Path is: %s\n", path)
 }
